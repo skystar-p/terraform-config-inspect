f0da6de136952b36590a7bcd714cfb58914ff1a6
Jaehyeon Park <skystar@skystar.dev>
2024-03-11 10:57:06 +0900

    change package name

--- default.nix
+++ default.nix
@@ -13,8 +13,8 @@
 }:
 
 buildGoApplication {
-  pname = "myapp";
-  version = "0.1";
+  pname = "terraform-config-inspect";
+  version = "0.0.0";
   pwd = ./.;
   src = ./.;
   modules = ./gomod2nix.toml;
